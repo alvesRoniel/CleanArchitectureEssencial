9dbfda9baa8097ece229e1d8c14f71aa9c7262a2
FAZENDAroniel.dasilva <roniel.dasilva@economia.df.gov.br>
2021-09-08 11:51:55 -0300

    Criando as entidades do modelo de domínio.

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Propriedade de Navegação
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stok { get; set; }
+        public string image { get; set; }
+
+        //Chave estrangeira 
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
