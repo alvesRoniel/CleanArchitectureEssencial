5e84497e6b3664c72804dd06f0866142de16ef70
alvesRoniel <roniel.alves@gmail.com>
2021-09-09 10:32:17 -0300

    Implementando o Repositório - CategoryRepository e ProductRepository

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        ApplicationDbContext _categoryContext;
+
+        public CategoryRepository(ApplicationDbContext categoryContext)
+        {
+            _categoryContext = categoryContext;
+        }
+
+
+        public async Task<Category> CreateCategoryAsync(Category category)
+        {
+             _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _categoryContext.DbCategories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int? id)
+        {
+            return await _categoryContext.DbCategories.FindAsync(id);
+        }
+
+        public async Task<Category> RemoveCategoryAsync(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateCategoryAsync(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        ApplicationDbContext _productContext;
+
+        public ProductRepository(ApplicationDbContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public async Task<Product> CreateProductAsync(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductiesAsync()
+        {
+            return await _productContext.DbProducts.ToListAsync();
+        }
+
+        public async Task<Product> GetProductCategoryByIdAsync(int? id)
+        {
+
+            //eager loading
+            return await _productContext.DbProducts
+                .Include(c => c.Category)
+                .SingleOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Product> RemoveProductAsync(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateProductAsync(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
