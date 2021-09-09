889aa2e9fe851b6f6f04d002430e1b83d19712d2
alvesRoniel <roniel.alves@gmail.com>
2021-09-09 09:47:10 -0300

    Configurando as entidades usando a Fluent API

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            /*Defini o relacionamento de um para muitos entre categoria e produto.
+              Onde a categoria pode ter muitos produtos e a chave estrangeira vai ser categoriaId.*/
+            builder.HasOne(c => c.Category).WithMany(p => p.Products).HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
