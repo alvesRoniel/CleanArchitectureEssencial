a3b40ed4966a37d83230a40536ce57605714ee0a
alvesRoniel <roniel.alves@gmail.com>
2021-09-09 21:04:04 -0300

    Criando os DTOs e os mapeamentos com AutoMapper

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "The {0} is Required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "The {0} is Required")]
+        [Column(TypeName ="decimal(18,2)")]
+        [DisplayFormat(DataFormatString ="{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+
+        [Required(ErrorMessage = "The {0} is Required")]
+        [Range(1,9999)]
+        [DisplayName("Stok")]
+        public int Stok { get; set; }
+
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
