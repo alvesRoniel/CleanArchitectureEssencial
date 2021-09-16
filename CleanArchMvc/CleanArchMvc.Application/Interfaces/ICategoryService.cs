20747d2d929ad74fc1245652acdd9ba6faa09da9
alvesRoniel <roniel.alves@gmail.com>
2021-09-16 18:09:28 -0300

    Implementando o serviço para Category - CategoryService

---
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        /// <summary>
+        //// Retorna todas as categorias
+        //// </summary>
+        //// <returns>Uma lista com todoas as categorias</returns>
+        Task<IEnumerable<CategoryDTO>> GetAllCategoriesAsync();
+
+        /// <summary>
+        /// Retorna apenas uma categoria pelo id informado
+        /// </summary>
+        /// <param name="id">Id da categoria</param>
+        /// <returns>Um objeto do tipo categoria</returns>
+        Task<CategoryDTO> GetCategoryByIdAsync(int? id);
+
+        /// <summary>
+        /// Adiciona uma nova categoria no banco
+        /// </summary>
+        /// <param name="categoryDto">Ojeto a ser criado</param>
+        /// <returns></returns>
+        Task AddCategoryAsync(CategoryDTO categoryDto);
+
+        /// <summary>
+        /// Atualiza os dados de uma categoria existente
+        /// </summary>
+        /// <param name="categoryDto">Ojeto a ser atualizado</param>
+        /// <returns></returns>
+        Task UpdateCategoryAsync(CategoryDTO categoryDto);
+
+        /// <summary>
+        /// Remove uma categoria do banco de dados
+        /// </summary>
+        /// <param name="id">Id da categoria a ser deletada</param>
+        /// <returns></returns>
+        Task RemoveCategoryAsync(int? id);
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetAllCategoriesAsync()
+        {
+            return _mapper.Map<IEnumerable<CategoryDTO>>(await _categoryRepository.GetAllCategoriesAsync());
+        }
+
+        public async Task<CategoryDTO> GetCategoryByIdAsync(int? id)
+        {
+            return _mapper.Map<CategoryDTO>(await _categoryRepository.GetCategoryByIdAsync(id));
+        }
+
+        public async Task AddCategoryAsync(CategoryDTO categoryDto)
+        {
+            var categoriEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.CreateCategoryAsync(categoriEntity);
+        }
+
+        public async Task UpdateCategoryAsync(CategoryDTO categoryDto)
+        {
+            var categoriEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.UpdateCategoryAsync(categoriEntity);
+        }
+
+        public async Task RemoveCategoryAsync(int? id)
+        {
+            var categoriEntity = _categoryRepository.GetCategoryByIdAsync(id).Result;
+            await _categoryRepository.RemoveCategoryAsync(categoriEntity);
+        }
+    }
+}
