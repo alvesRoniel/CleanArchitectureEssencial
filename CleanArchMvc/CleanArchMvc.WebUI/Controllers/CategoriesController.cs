2c1db4a88cb86854e9352d27bccdeae0d0ced782
alvesRoniel <roniel.alves@gmail.com>
2021-09-16 21:19:22 -0300

    Registrando os serviços e criando o Controller e View para Category.

---
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoruyService;
+
+        public CategoriesController(ICategoryService categoruyService)
+        {
+            _categoruyService = categoruyService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoruyService.GetAllCategoriesAsync();
+            return View(categories);
+        }
+    }
+}
