794f8a239efae49d0ae9569cfd6e4ed7f47e0469
alvesRoniel <roniel.alves@gmail.com>
2021-09-25 21:03:15 -0300

    Criando o controlador AccountController e as ViewModels na camada WebUI

---
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+
+
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="{0} is required")]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
