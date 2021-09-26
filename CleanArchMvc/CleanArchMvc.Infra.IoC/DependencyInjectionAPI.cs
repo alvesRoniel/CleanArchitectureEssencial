93ad660d59ad436fdeca5dece0b159907ad3028e
alvesRoniel <roniel.alves@gmail.com>
2021-09-26 14:52:27 -0300

    Configurando o projeto CleanArchMvc.API

---
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            //CONFIGURAÇÃO PARA O IDENTITY
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            //CONFIGURAÇÃO PARA O USO DA APLICAÇÃO
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            //CONFIGURAÇÃO PARA O USO DO AUTOMAPPER
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            //CONFIGURAÇÃO PARA O USO DO CQRS/MEDIAT_R
+            var myHandler = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myHandler);
+
+            return services;
+        }
+    }
+}
