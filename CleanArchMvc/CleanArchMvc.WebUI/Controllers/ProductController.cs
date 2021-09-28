7d75011f535baee9576f11058277df6b2534af65
FAZENDAroniel.dasilva <roniel.dasilva@economia.df.gov.br>
2021-09-28 15:01:39 -0300

    Upload de Imagens

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductController.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -15,9 +18,10 @@ namespace CleanArchMvc.WebUI.Controllers
         private readonly ICategoryService _categoryService;
         private readonly IWebHostEnvironment _environment;
 
-        public ProductController(IProductService productService, 
-                                 ICategoryService categoryService, 
-                                 IWebHostEnvironment environment)
+        public ProductController(IProductService productService,
+                                 ICategoryService categoryService,
+                                 IWebHostEnvironment environment
+        )
         {
             _productService = productService;
             _categoryService = categoryService;
@@ -57,12 +61,23 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             if(ModelState.IsValid)
             {
+                if (HttpContext.Request.Form.Files != null)
+                {
+                    var file = Request.Form.Files[0];
+                    if(file.Length > 0)
+                    {
+                        productDTO.Image = await SaveImage(file);
+                    }
+                }
+
                 await _productService.AddProductAsync(productDTO);
                 return RedirectToAction(nameof(Index));
             }
 
             return View(productDTO);
         }
+
+       
 
         /// <summary>
         /// Carrega um produto para realizar a edição.
@@ -73,6 +88,7 @@ namespace CleanArchMvc.WebUI.Controllers
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null) return NotFound();
+
             var productsDto = await _productService.GetProductsByIdAsync(id);
 
             if (productsDto == null) return NotFound();
@@ -90,10 +106,21 @@ namespace CleanArchMvc.WebUI.Controllers
         /// <param name="productDTO">Obj produto a ser editdo</param>
         /// <returns>Obj Produto com os dados atualizados</returns>
         [HttpPost()]
-        public async Task<IActionResult> Edit(ProductDTO productDTO)
+        public async Task<IActionResult> Edit(ProductDTO productDTO, string image)
         {
             if (ModelState.IsValid)
             {
+
+                if (HttpContext.Request.Form.Files != null)
+                {
+                    var file = Request.Form.Files[0];
+                    if (file.Length > 0)
+                    {
+                        DeleteImage(image);
+                        productDTO.Image = await SaveImage(file);
+                    }
+                }
+
                 await _productService.UpdateProductAsync(productDTO);
                 return RedirectToAction(nameof(Index));
             }
@@ -139,11 +166,36 @@ namespace CleanArchMvc.WebUI.Controllers
             if (productDto == null) return NotFound();
 
             var wweroot = _environment.WebRootPath;
-            var image = Path.Combine(wweroot, "images\\" + productDto.Image);
+            var image = Path.Combine(wweroot, "Resources\\Images\\" + productDto.Image);
             var exists = System.IO.File.Exists(image);
             ViewBag.ImageExist = exists;
 
             return View(productDto);
+        }
+
+        [NonAction]
+        private void DeleteImage(string imageName)
+        {
+            var wweroot = _environment.WebRootPath;
+            var imagePath = Path.Combine(wweroot, "Resources\\Images\\" + imageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+        }
+
+        private async Task<string> SaveImage(Microsoft.AspNetCore.Http.IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
+
+            imageName = $"{imageName}{DateTime.UtcNow.ToString("yymmssfff")}{Path.GetExtension(imageFile.FileName)}";
+
+            var imagePath = Path.Combine(_environment.ContentRootPath, @"wwwroot/Resources/Images", imageName);
+
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            return imageName;
         }
     }
 }
