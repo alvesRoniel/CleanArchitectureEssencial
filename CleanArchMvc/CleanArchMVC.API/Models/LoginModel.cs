9d1c8b1b4285a6f5e3f039e3877c0e7b89051fb5
alvesRoniel <roniel.alves@gmail.com>
2021-10-02 15:40:30 -0300

    Configurando as informações do token e os DTOs usados

---
+++ CleanArchMvc/CleanArchMVC.API/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMVC.API.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
